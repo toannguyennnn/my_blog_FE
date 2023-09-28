f41682c87733aa0d84f48aad0030cb718d4794ab
toan nguyen <toan98lsbg@gmail.com>
2023-09-28 23:18:49 +0700

    edit

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,35 +1,38 @@
 <template>
-  <v-app-bar elevation="2" class="position-relative">
+  <v-app-bar elevation="2" class="app-navbar position-relative">
     <span class="d-md-none">
-      <v-app-bar-nav-icon @click="sidebar = !sidebar"> </v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="sidebar = !sidebar"></v-app-bar-nav-icon>
     </span>
-    <v-app-bar-title><router-link to="/" class="text-decoration-none text-black">My Blogs</router-link> </v-app-bar-title>
-    <v-list nav class="d-flex">
-      <v-list-item
-        class="hidden-sm-and-down"
-        v-for="item in menuItems"
-        :key="item.title"
-        :title="item.title"
-        :prepend-icon="item.icon"
-        :value="item.title"
-        @click="routerHandler(item.path)"
-      >
-      </v-list-item></v-list
-  ></v-app-bar>
 
-  <!-- <v-navigation-drawer v-model="sidebar" temporary>
-    <v-list nav>
-      <v-list-item
-        v-for="item in menuItems"
-        :key="item.title"
-        :title="item.title"
-        :prepend-icon="item.icon"
-        :value="item.title"
-        @click="routerHandler(item.path)"
-      >
+    <v-list nav class="d-flex">
+      <v-list-item>
+        <router-link to="/" class="text-decoration-none text-black font-weight-bold d-flex">
+          <v-img src="../../assets/logo.svg" width="20"></v-img>
+          <span class="ms-1">My Blogs</span>
+        </router-link>
+      </v-list-item>
+
+      <v-list-item class="hidden-sm-and-down ms-7" v-for="item in menuItems2" :key="item.title" :title="item.title"
+        :prepend-icon="item.icon" :value="item.title" @click="routerHandler(item.path)">
       </v-list-item>
     </v-list>
-  </v-navigation-drawer> -->
+
+    <v-list nav class="d-flex">
+      <v-text-field label="Search" class="nav-search me-2" hide-details density="compact" append-inner-icon="mdi-magnify">
+      </v-text-field>
+      <v-list-item class="hidden-sm-and-down me-2" v-for="item in menuItems" :key="item.title" :title="item.title"
+        :prepend-icon="item.icon" :value="item.title" @click="routerHandler(item.path)">
+      </v-list-item>
+    </v-list>
+  </v-app-bar>
+
+  <v-navigation-drawer v-model="sidebar" temporary>
+    <v-list nav>
+      <v-list-item v-for="item in menuItems3" :key="item.title" :title="item.title" :prepend-icon="item.icon"
+        :value="item.title" @click="routerHandler(item.path)">
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <script>
@@ -40,10 +43,23 @@ export default {
       appTitle: "Awesome App",
       sidebar: false,
       menuItems: [
-        { title: "Create Blog", path: "/create", icon: "mdi-plus" },
         { title: "Sign Up", path: "/sign-up", icon: "mdi-account-plus" },
         { title: "Sign In", path: "#", icon: "mdi-login" },
       ],
+      menuItems2: [
+        { title: "Blogs", path: "#" },
+        { title: "Categories", path: "#" },
+        { title: "Support", path: "#" },
+        { title: "Introduction", path: "#" },
+      ],
+      menuItems3: [
+        { title: "Blogs", path: "#" },
+        { title: "Categories", path: "#" },
+        { title: "Support", path: "#" },
+        { title: "Introduction", path: "#" },
+        { title: "Sign Up", path: "/sign-up", icon: "mdi-account-plus" },
+        { title: "Sign In", path: "#", icon: "mdi-login" },
+      ]
     };
   },
   methods: {
@@ -53,4 +69,17 @@ export default {
   },
 };
 </script>
-<style></style>
+<style>
+.v-list-item__prepend>.v-icon {
+  margin-right: 5px !important;
+}
+
+.v-toolbar__content,
+.v-toolbar__extension {
+  justify-content: space-between;
+}
+
+.nav-search .v-field.v-field--appended {
+  width: 160px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,122 @@
 <template>
   <v-container fluid>
     <v-row>
-      <v-col class="text-center">
-        <h3>Homepage</h3>
+      <v-col cols="10">
+        <v-row>
+          <v-col>
+            <h3>Tourism</h3>
+            <v-divider thickness="2"></v-divider>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col cols="7" class="d-flex border">
+            <v-img src="../assets/logo.svg" alt="" width="500" height="200"></v-img>
+          </v-col>
+          <v-col cols="5">
+            <div class="text-justify">
+              <h3>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius
+                nemo maiores quas
+              </h3>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Aliquam modi deleniti quo maxime atque voluptate qui mollitia,
+                velit consectetur, officiis fuga corporis officia neque debitis
+                quos quisquam sunt labore! Debitis!
+              </p>
+            </div>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col>
+            <div class="d-flex">
+              <v-img src="../assets/logo.svg" alt="" width="60"></v-img>
+              <span class="text-justify ms-2">
+                <h4>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit
+                </h4>
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa
+                  laudantium aliquid soluta saepe aut
+                </p>
+              </span>
+            </div>
+          </v-col>
+          <v-col>
+            <div class="d-flex">
+              <v-img src="../assets/logo.svg" alt="" width="60"></v-img>
+              <span class="text-justify ms-2">
+                <h4>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit
+                </h4>
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa
+                  laudantium aliquid soluta saepe aut
+                </p>
+              </span>
+            </div>
+          </v-col>
+          <v-col>
+            <div class="d-flex">
+              <v-img src="../assets/logo.svg" alt="" width="60"></v-img>
+              <span class="text-justify ms-2">
+                <h4>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit
+                </h4>
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa
+                  laudantium aliquid soluta saepe aut
+                </p>
+              </span>
+            </div>
+          </v-col>
+        </v-row>
+
+        <v-divider class="my-3"></v-divider>
+      </v-col>
+
+      <v-divider vertical inset></v-divider>
+
+      <v-col cols="2">
+        <div>
+          <v-row>
+            <v-col class="text-center">
+              <h4>Most popular blogs</h4>
+            </v-col>
+          </v-row>
+
+          <v-row v-for="n in 3" v-bind:key="n">
+            <v-col cols="3" class="d-flex align-center">
+              <v-img src="../assets/logo.svg" alt=""></v-img>
+            </v-col>
+            <v-col cols="9" class="text-justify py-0">
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              </p>
+            </v-col>
+            <div class="w-100 text-right me-2">
+              <v-icon icon="mdi-eye" size="x-small"></v-icon>
+              <span class="text-body-2">10.000</span>
+            </div>
+            <v-divider class="mx-3"></v-divider>
+          </v-row>
+        </div>
+
+        <div class="mt-6">
+          <v-card>
+            <v-card-title class="text-body-1 font-weight-bold text-center">Email Subscription</v-card-title>
+            <v-card-text>
+              <p>Subscribe to receive the lastest information!</p>
+              <v-text-field label="Name" hide-details density="compact"></v-text-field>
+              <v-text-field label="Email" class="mt-2" density="compact" hide-details></v-text-field>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn block class="bg-blue text-none">Subscribe</v-btn>
+            </v-card-actions>
+          </v-card>
+        </div>
       </v-col>
     </v-row>
   </v-container>
